8264cd548977379c393a592122425caf7b5d98e6
Tyler <tmayoff@gmail.com>
2018-03-22 21:22:11 -0400

    .

--- EduPlanner/EduPlanner/MainWindow.xaml.cs
+++ EduPlanner/EduPlanner/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace EduPlanner {
     /// <summary>
@@ -22,6 +24,10 @@ namespace EduPlanner {
         Schedule schedule;
         Data data;
 
+        DispatcherTimer timer = new DispatcherTimer();
+
+        int timerIntervalMin = 5;
+
         public MainWindow() {
             InitializeComponent();
 
@@ -31,6 +37,11 @@ namespace EduPlanner {
             schedule = DataManager.schedule;
             DataManager.mainWindow = this;
             UpdateAgendaView();
+
+            //Timer
+            Refresh(this, EventArgs.Empty);
+            timer.Tick += new EventHandler(Refresh);
+            timer.Interval = new TimeSpan(0, timerIntervalMin, 0);
         }
 
         public void UpdateAgendaView() {
@@ -52,6 +63,43 @@ namespace EduPlanner {
             }
         }
 
+        #region Event Handlers
+
+        public void Refresh(object sender, EventArgs e) {
+            DateTime currentDateTime = DateTime.Today;
+
+            for (int i = 0; i < AgendaView.Children.Count; i++) {
+
+                DayCard dayCard = AgendaView.Children[i] as DayCard;
+                Day day = dayCard.day;
+                Card dayCardCard = dayCard.FindName("Card") as Card;
+
+                if (day.day == DateTime.Today.DayOfWeek) {
+                    dayCardCard.Background = Brushes.LightBlue;
+
+                    StackPanel classesPanel = dayCard.FindName("ClassesView") as StackPanel;
+
+                    for (int j = 0; j < classesPanel.Children.Count; j++) {
+
+                        ClassCard classCard = classesPanel.Children[j] as ClassCard;
+                        Class _class = classCard._class;
+                        Card classCardCard = classCard.FindName("Card") as Card;
+                        DateTime start = _class.classTimes[day.day][0].Value;
+                        DateTime end = _class.classTimes[day.day][1].Value;
+
+                        if (currentDateTime >= start && currentDateTime <= end) {
+                            classCardCard.Background = Brushes.LightGreen;
+                            txtCurrentClass.Text = _class.className;
+                        } else {
+                            classCardCard.Background = Brushes.White;
+                            txtCurrentClass.Text = "";
+                        }
+                    }
+                } else
+                    dayCardCard.Background = Brushes.White;
+            }
+        }
+
         private void BtnAddClass_Click(object sender, RoutedEventArgs e) {
             AddClass addClass = new AddClass();
             addClass.Closed += new EventHandler(WindowAddEditClass_Closed);
@@ -70,5 +118,13 @@ namespace EduPlanner {
         public void WindowAddEditClass_Closed(object sender, EventArgs e) {
             UpdateAgendaView();
         }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            BtnSave(sender, new RoutedEventArgs());
+        }
+
+        #endregion
+
+
     }
 }
