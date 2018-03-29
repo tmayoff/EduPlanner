f8ca866f05cfee341c5d703b8dcdaeb4745d9b59
Jogoyo <joseph@josephdp.com>
2018-03-29 19:36:07 -0400

    Updated window names

--- EduPlanner/EduPlanner/Updater.cs
+++ EduPlanner/EduPlanner/Updater.cs
@@ -2,18 +2,22 @@
 using System.Xml;
 using System.Windows;
 
-namespace EduPlanner {
-    public static class Updater {
+namespace EduPlanner
+{
+    public static class Updater
+    {
         static string xmlUrl = "https://raw.githubusercontent.com/tyxman/EduPlanner/master/EduPlanner/update.xml";
         static string downloadUrl = "https://github.com/tyxman/EduPlanner/releases/";
         static string mbHeader = DataManager.APPLICATIONNAME + " Updater";
         static string msgError = "An unknown error occured while checking for updates.";
 
-        public static void CheckForUpdate(bool startup = false) {
+        public static void CheckForUpdate(bool startup = false)
+        {
             string newVersion = string.Empty;
 
             XmlTextReader reader = new XmlTextReader(xmlUrl);
-            try {
+            try
+            {
                 // simply (and easily) skip the junk at the beginning
                 reader.MoveToContent();
 
@@ -26,17 +30,24 @@ namespace EduPlanner {
 
                 // we check if the xml starts with a proper  
                 // "ourfancyapp" element node  
-                if (reader.NodeType == XmlNodeType.Element && reader.Name == "EduPlanner") {
-                    while (reader.Read()) {
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "EduPlanner")
+                {
+                    while (reader.Read())
+                    {
                         // when we find an element node,  
                         // we remember its name  
-                        if (reader.NodeType == XmlNodeType.Element) {
+                        if (reader.NodeType == XmlNodeType.Element)
+                        {
                             elementName = reader.Name;
-                        } else {
+                        }
+                        else
+                        {
                             // for text nodes...  
-                            if (reader.NodeType == XmlNodeType.Text && reader.HasValue) {
+                            if (reader.NodeType == XmlNodeType.Text && reader.HasValue)
+                            {
                                 // we check what the name of the node was  
-                                switch (elementName) {
+                                switch (elementName)
+                                {
                                     case "version":
                                         newVersion = reader.Value;
                                         break;
@@ -44,7 +55,8 @@ namespace EduPlanner {
                                         downloadUrl = reader.Value;
                                         break;
                                     case "release":
-                                        if (reader.Value == "no" && !startup) {
+                                        if (reader.Value == "no" && !startup)
+                                        {
                                             throw new Exception("An update was found, but it has not yet been released.");
                                         }
                                         break;
@@ -52,33 +64,47 @@ namespace EduPlanner {
                             }
                         }
                     }
-                } else {
+                }
+                else
+                {
                     throw new Exception(msgError);
                 }
 
                 string curVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(2);
 
-                if (curVersion.CompareTo(newVersion) < 0) {
+                if (curVersion.CompareTo(newVersion) < 0)
+                {
                     string msgText = String.Format("New version detected. Would you like to download it now?\n\n" +
                                                    "Current version: {0}\n" +
                                                    "New version: {1}", curVersion, newVersion);
 
                     MessageBoxResult result = MessageBox.Show(msgText, mbHeader, MessageBoxButton.YesNo, MessageBoxImage.Question);
-                    if (result == MessageBoxResult.Yes) {
+                    if (result == MessageBoxResult.Yes)
+                    {
                         System.Diagnostics.Process.Start(downloadUrl);
                     }
-                } else if (!startup) {
-                    if (curVersion == newVersion) {
+                }
+                else if (!startup)
+                {
+                    if (curVersion == newVersion)
+                    {
                         MessageBox.Show("You are running the latest version of EduPlanner!", mbHeader, MessageBoxButton.OK, MessageBoxImage.Information);
-                    } else {
+                    }
+                    else
+                    {
                         throw new Exception(msgError);
                     }
                 }
 
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 MessageBox.Show(ex.Message, mbHeader, MessageBoxButton.OK, MessageBoxImage.Error);
-            } finally {
-                if (reader != null) {
+            }
+            finally
+            {
+                if (reader != null)
+                {
                     reader.Close();
                 }
             }
