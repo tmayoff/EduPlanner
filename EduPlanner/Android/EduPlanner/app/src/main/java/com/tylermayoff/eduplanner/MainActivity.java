4364195723c9a41d301509c4f22069ccd47bece2
Tyler <tmayoff@gmail.com>
2018-03-31 20:14:50 -0400

    Began android application

---
+++ EduPlanner/Android/EduPlanner/app/src/main/java/com/tylermayoff/eduplanner/MainActivity.java
@@ -0,0 +1,21 @@
+package com.tylermayoff.eduplanner;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void BtnAddClass_Click(View view) {
+//        startActivity(new Intent(this, AddClass.class));
+    }
+}
